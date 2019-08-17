cd7117196df87fb76e23b58d40ab71a7ca4a0936
Dafna Hirschfeld <dafna3@gmail.com>
2019-08-17 04:57:32 -0700

    first

---
+++ configfs-gen-orig-conf.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -i
+set -x
+
+echo "file drivers/media/platform/vimc/* +p" > /sys/kernel/debug/dynamic_debug/control
+echo 15 > /proc/sys/kernel/printk
+
+mount -t configfs none /configfs
+mkdir /configfs/vimc/mdev
+mkdir "/configfs/vimc/mdev/entities/vimc-sensor:Sensor A"
+mkdir "/configfs/vimc/mdev/entities/vimc-sensor:Sensor B"
+mkdir "/configfs/vimc/mdev/entities/vimc-debayer:Debayer A"
+mkdir "/configfs/vimc/mdev/entities/vimc-debayer:Debayer B"
+mkdir "/configfs/vimc/mdev/entities/vimc-capture:Raw Capture 0"
+mkdir "/configfs/vimc/mdev/entities/vimc-capture:Raw Capture 1"
+mkdir "/configfs/vimc/mdev/entities/vimc-sensor:RGB/YUV Input"
+mkdir "/configfs/vimc/mdev/entities/vimc-scaler:Scaler"
+mkdir "/configfs/vimc/mdev/entities/vimc-capture:RGB/YUV Capture"
+
+mkdir "/configfs/vimc/mdev/links/my-sensor:0->my-capture:0"
+echo 3 > "/configfs/vimc/mdev/links/my-sensor:0->my-capture:0/flags"
+echo 1 > /configfs/vimc/mdev/hotplug
---
+++ install_vimc.sh
@@ -0,0 +1,25 @@
+if [ "$#" -ne 1 ]; then
+echo "need kernel version"
+exit 1
+fi
+
+version=$1
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/platform/vimc ; cp drivers/media/platform/vimc/vimc.ko /lib/modules/$version/kernel/drivers/media/platform/vimc ; true /lib/modules/$version/kernel/drivers/media/platform/vimc/vimc.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/platform/vimc/vimc.ko  ; true /lib/modules/$version/kernel/drivers/media/platform/vimc/vimc.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media ; cp drivers/media/media.ko /lib/modules/$version/kernel/drivers/media ; true /lib/modules/$version/kernel/drivers/media/media.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/media.ko  && true /lib/modules/$version/kernel/drivers/media/media.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/platform/vicodec
+cp drivers/media/platform/vicodec/vicodec.ko /lib/modules/$version/kernel/drivers/media/platform/vicodec
+true /lib/modules/$version/kernel/drivers/media/platform/vicodec/vicodec.ko
+scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/platform/vicodec/vicodec.ko  && true /lib/modules/$version/kernel/drivers/media/platform/vicodec/vicodec.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/v4l2-core ; cp drivers/media/v4l2-core/v4l2-mem2mem.ko /lib/modules/$version/kernel/drivers/media/v4l2-core ; true /lib/modules/$version/kernel/drivers/media/v4l2-core/v4l2-mem2mem.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/v4l2-core/v4l2-mem2mem.ko  && true /lib/modules/$version/kernel/drivers/media/v4l2-core/v4l2-mem2mem.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/v4l2-core ; cp drivers/media/v4l2-core/videodev.ko /lib/modules/$version/kernel/drivers/media/v4l2-core ; true /lib/modules/$version/kernel/drivers/media/v4l2-core/videodev.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/v4l2-core/videodev.ko  && true /lib/modules/$version/kernel/drivers/media/v4l2-core/videodev.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/common/videobuf2 ; cp drivers/media/common/videobuf2/videobuf2-v4l2.ko /lib/modules/$version/kernel/drivers/media/common/videobuf2 ; true /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-v4l2.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-v4l2.ko  && true /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-v4l2.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/common/videobuf2 ; cp drivers/media/common/videobuf2/videobuf2-vmalloc.ko /lib/modules/$version/kernel/drivers/media/common/videobuf2 ; true /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-vmalloc.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-vmalloc.ko  && true /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-vmalloc.ko
+
+mkdir -p /lib/modules/$version/kernel/drivers/media/common/videobuf2 ; cp drivers/media/common/videobuf2/videobuf2-common.ko /lib/modules/$version/kernel/drivers/media/common/videobuf2 ; true /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-common.ko ; scripts/sign-file "sha512" "certs/signing_key.pem" certs/signing_key.x509 /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-common.ko  && true /lib/modules/$version/kernel/drivers/media/common/videobuf2/videobuf2-common.ko
