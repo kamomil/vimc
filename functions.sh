153936bfd5c099f4c9c54cba4bd618fa3c1b33c4
Dafna Hirschfeld <dafna.hirschfeld@collabora.com>
2019-09-20 14:11:04 +0300

    move the functions to different file

---
+++ functions.sh
@@ -0,0 +1,101 @@
+function reinstall_vimc {
+	find /configfs/ -mindepth 6 -name "pad:sink*" -exec unlink {} \;
+	find /configfs/ -mindepth 5 -maxdepth 5 -exec rmdir {} \;
+	find /configfs/ -mindepth 3 -maxdepth 3 -type d -exec rmdir {} \;
+	find /configfs/ -mindepth 2 -maxdepth 2 -type d -exec rmdir {} \;
+	modprobe -vr vimc
+	umount /configfs
+	modprobe -v vimc
+	mount -t configfs none /configfs
+	# This produce to much debugs when streaming on debayer and scaler
+	echo "file drivers/media/platform/vimc/* +p" > /sys/kernel/debug/dynamic_debug/control
+	# echo "file drivers/media/mc/* +p" > /sys/kernel/debug/dynamic_debug/control
+	# echo "file drivers/media/platform/vimc/vimc-core.c +p" > /sys/kernel/debug/dynamic_debug/control
+}
+function simpler_topo {
+	echo "start simple topo"
+	DEV2=$1
+	# Creating the entities
+	mkdir "/configfs/vimc/$DEV2"
+	mkdir "/configfs/vimc/$DEV2/vimc-sensor:sen"
+	mkdir "/configfs/vimc/$DEV2/vimc-capture:cap-sen" #/dev/video0
+
+	#sen -> cap-sen
+	mkdir "/configfs/vimc/$DEV2/vimc-sensor:sen/pad:source:0/to-cap"
+	ln -s "/configfs/vimc/$DEV2/vimc-capture:cap-sen/pad:sink:0" "/configfs/vimc/$DEV2/vimc-sensor:sen/pad:source:0/to-cap"
+	echo on > "/configfs/vimc/$DEV2/vimc-sensor:sen/pad:source:0/to-cap/immutable"
+	echo on > "/configfs/vimc/$DEV2/vimc-sensor:sen/pad:source:0/to-cap/enabled"
+}
+
+function simple_topo {
+	echo "start simple topo for device "
+	DEV=$1
+# Creating the entities
+	mkdir "/configfs/vimc/$DEV"
+	mkdir "/configfs/vimc/$DEV/vimc-sensor:sen"
+	mkdir "/configfs/vimc/$DEV/vimc-debayer:deb"
+	mkdir "/configfs/vimc/$DEV/vimc-scaler:sca"
+	mkdir "/configfs/vimc/$DEV/vimc-capture:cap-sca" #/dev/video2
+	mkdir "/configfs/vimc/$DEV/vimc-capture:cap-sen" #/dev/video1
+	mkdir "/configfs/vimc/$DEV/vimc-capture:cap-deb" #/dev/video0
+
+# Creating the links
+#sen -> deb
+	mkdir "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-deb"
+	ln -s "/configfs/vimc/$DEV/vimc-debayer:deb/pad:sink:0" "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-deb"
+	echo on > "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-deb/immutable"
+	echo on > "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-deb/enabled"
+
+#deb -> sca
+	mkdir "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-sca"
+	ln -s "/configfs/vimc/$DEV/vimc-scaler:sca/pad:sink:0" "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-sca"
+	echo on > "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-sca/immutable"
+	echo on > "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-sca/enabled"
+
+#sca -> cap-sca
+	mkdir "/configfs/vimc/$DEV/vimc-scaler:sca/pad:source:1/to-cap"
+	ln -s "/configfs/vimc/$DEV/vimc-capture:cap-sca/pad:sink:0" "/configfs/vimc/$DEV/vimc-scaler:sca/pad:source:1/to-cap"
+	echo on > "/configfs/vimc/$DEV/vimc-scaler:sca/pad:source:1/to-cap/immutable"
+	echo on > "/configfs/vimc/$DEV/vimc-scaler:sca/pad:source:1/to-cap/enabled"
+
+#sen -> cap-sen
+	mkdir "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-cap"
+	ln -s "/configfs/vimc/$DEV/vimc-capture:cap-sen/pad:sink:0" "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-cap"
+	echo on > "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-cap/immutable"
+	echo on > "/configfs/vimc/$DEV/vimc-sensor:sen/pad:source:0/to-cap/enabled"
+
+#deb -> cap-deb
+	mkdir "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-cap"
+	ln -s "/configfs/vimc/$DEV/vimc-capture:cap-deb/pad:sink:0" "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-cap"
+	echo on > "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-cap/immutable"
+	echo on > "/configfs/vimc/$DEV/vimc-debayer:deb/pad:source:1/to-cap/enabled"
+}
+
+function configure_all_formats {
+	SEN_0='"sen":0[fmt:SBGGR8_1X8/640x480]'
+	DEB_0='"deb":0[fmt:SBGGR8_1X8/640x480]'
+	DEB_1='"deb":1[fmt:RGB888_1X24/640x480]'
+	SCA_0='"sca":0[fmt:RGB888_1X24/640x480]'
+	SCA_1='"sca":1[fmt:RGB888_1X24/640x480]'
+
+#	media-ctl -d platform:vimc-000 -V '"sen":0[fmt:SBGGR8_1X8/640x480],"deb":0[fmt:SBGGR8_1X8/640x480]'
+# media-ctl -d platform:vimc-000 -V '"deb":0[fmt:SBGGR8_1X8/640x480]'
+# This is actually the default and the only supported format for deb:1
+# see `v4l2-ctl -d /dev/v4l-subdev1 --list-subdev-mbus 1`
+#	media-ctl -d platform:vimc-000 -V '"deb":1[fmt:RGB888_1X24/640x480]'
+#	media-ctl -d platform:vimc-000 -V '"sca":0[fmt:RGB888_1X24/640x480]'
+#	media-ctl -d platform:vimc-000 -V '"sca":1[fmt:RGB888_1X24/640x480]'
+	media-ctl -d platform:vimc-000 -V "${SEN_0},${DEB_0},${DEB_1},${SCA_0},${SCA_1}"
+	v4l2-ctl -z platform:vimc-000 -d "cap-sen" -v pixelformat=BA81
+	v4l2-ctl -z platform:vimc-000 -d "cap-deb" -v pixelformat=RGB3
+#The scaler scales times 3, so need to set its capture accordingly
+	v4l2-ctl -z platform:vimc-000 -d "cap-sca" -v pixelformat=RGB3,width=1920,height=1440
+}
+
+
+function configure_all_formats_simpler {
+	media-ctl -d platform:vimc-001 -V '"sen":0[fmt:SBGGR8_1X8/640x480]'
+	v4l2-ctl -z platform:vimc-001 -d "cap-sen" -v pixelformat=BA81
+}
+
+
--- sym-unit-tests-simple-topo.sh
+++ sym-unit-tests-simple-topo.sh
@@ -2,26 +2,12 @@
 # Display commands and their arguments as they are executed.
 # set -x
 
-function reinstall_vimc {
-	find /configfs/ -mindepth 6 -name "pad:sink*" -exec unlink {} \;
-	find /configfs/ -name "to-*" -exec rmdir {} \;
-	find /configfs/ -mindepth 5 -maxdepth 5 -exec rmdir {} \;
-	find /configfs/ -mindepth 3 -maxdepth 3 -type d -exec rmdir {} \;
-	find /configfs/ -mindepth 2 -maxdepth 2 -type d -exec rmdir {} \;
-	modprobe -vr vimc
-	umount /configfs
-	modprobe -v vimc
-	mount -t configfs none /configfs
-	# This produce to much debugs when streaming on debayer and scaler
-	# echo "file drivers/media/platform/vimc/* +p" > /sys/kernel/debug/dynamic_debug/control
-	# echo "file drivers/media/mc/* +p" > /sys/kernel/debug/dynamic_debug/control
-	# echo "file drivers/media/platform/vimc/vimc-core.c +p" > /sys/kernel/debug/dynamic_debug/control
-}
-
 # we set all links to be enabled and immutable
 # therefore, when streaming we should make sure that
 # so, if trying to stream the sensor capture, we should
 # still make sure that also the formats in all other links match
+
+source ./functions.sh
 
 VIDSCA=/dev/video2
 VIDSEN=/dev/video1
@@ -30,87 +16,11 @@ STRM_CNT=10
 # generates the v4l-ctl streaming output "<<<<<<" ...
 STRM_OUT=$(printf '<%.0s' `seq $STRM_CNT`)
 
-function simpler_topo {
-	echo "start simple topo"
-	# Creating the entities
-	mkdir "/configfs/vimc/mdev2"
-	mkdir "/configfs/vimc/mdev2/vimc-sensor:sen"
-	mkdir "/configfs/vimc/mdev2/vimc-capture:cap-sen" #/dev/video0
-	
-	#sen -> cap-sen
-	mkdir "/configfs/vimc/mdev2/vimc-sensor:sen/pad:source:0/to-cap"
-	ln -s "/configfs/vimc/mdev2/vimc-capture:cap-sen/pad:sink:0" "/configfs/vimc/mdev2/vimc-sensor:sen/pad:source:0/to-cap"
-	echo on > "/configfs/vimc/mdev2/vimc-sensor:sen/pad:source:0/to-cap/immutable"
-	echo on > "/configfs/vimc/mdev2/vimc-sensor:sen/pad:source:0/to-cap/enabled"
-}
-
-function simple_topo {
-	echo "start simple topo for device "
-	# Creating the entities
-	mkdir "/configfs/vimc/mdev"
-	mkdir "/configfs/vimc/mdev/vimc-sensor:sen"
-	mkdir "/configfs/vimc/mdev/vimc-debayer:deb"
-	mkdir "/configfs/vimc/mdev/vimc-scaler:sca"
-	mkdir "/configfs/vimc/mdev/vimc-capture:cap-sca" #/dev/video2
-	mkdir "/configfs/vimc/mdev/vimc-capture:cap-sen" #/dev/video1
-	mkdir "/configfs/vimc/mdev/vimc-capture:cap-deb" #/dev/video0
-
-	# Creating the links
-	#sen -> deb
-	mkdir "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-deb"
-	ln -s "/configfs/vimc/mdev/vimc-debayer:deb/pad:sink:0" "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-deb"
-	echo on > "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-deb/immutable"
-	echo on > "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-deb/enabled"
-
-	#deb -> sca
-	mkdir "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-sca"
-	ln -s "/configfs/vimc/mdev/vimc-scaler:sca/pad:sink:0" "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-sca"
-	echo on > "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-sca/immutable"
-	echo on > "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-sca/enabled"
-
-	#sca -> cap-sca
-	mkdir "/configfs/vimc/mdev/vimc-scaler:sca/pad:source:1/to-cap"
-	ln -s "/configfs/vimc/mdev/vimc-capture:cap-sca/pad:sink:0" "/configfs/vimc/mdev/vimc-scaler:sca/pad:source:1/to-cap"
-	echo on > "/configfs/vimc/mdev/vimc-scaler:sca/pad:source:1/to-cap/immutable"
-	echo on > "/configfs/vimc/mdev/vimc-scaler:sca/pad:source:1/to-cap/enabled"
-
-	#sen -> cap-sen
-	mkdir "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-cap"
-	ln -s "/configfs/vimc/mdev/vimc-capture:cap-sen/pad:sink:0" "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-cap"
-	echo on > "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-cap/immutable"
-	echo on > "/configfs/vimc/mdev/vimc-sensor:sen/pad:source:0/to-cap/enabled"
-
-	#deb -> cap-deb
-	mkdir "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-cap"
-	ln -s "/configfs/vimc/mdev/vimc-capture:cap-deb/pad:sink:0" "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-cap"
-	echo on > "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-cap/immutable"
-	echo on > "/configfs/vimc/mdev/vimc-debayer:deb/pad:source:1/to-cap/enabled"
-}
-
-function configure_all_formats {
-	media-ctl -d platform:vimc-000 -V '"sen":0[fmt:SBGGR8_1X8/640x480]'
-	media-ctl -d platform:vimc-000 -V '"deb":0[fmt:SBGGR8_1X8/640x480]'
-	# This is actually the default and the only supported format for deb:1
-	# see `v4l2-ctl -d /dev/v4l-subdev1 --list-subdev-mbus 1`
-	media-ctl -d platform:vimc-000 -V '"deb":1[fmt:RGB888_1X24/640x480]'
-	media-ctl -d platform:vimc-000 -V '"sca":0[fmt:RGB888_1X24/640x480]'
-	media-ctl -d platform:vimc-000 -V '"sca":1[fmt:RGB888_1X24/640x480]'
-	v4l2-ctl -z platform:vimc-000 -d "cap-sen" -v pixelformat=BA81
-	v4l2-ctl -z platform:vimc-000 -d "cap-deb" -v pixelformat=RGB3
-	#The scaler scales times 3, so need to set its capture accordingly
-	v4l2-ctl -z platform:vimc-000 -d "cap-sca" -v pixelformat=RGB3,width=1920,height=1440
-}
-
-function configure_all_formats1 {
-	media-ctl -d platform:vimc-001 -V '"sen":0[fmt:SBGGR8_1X8/640x480]'
-	v4l2-ctl -z platform:vimc-001 -d "cap-sen" -v pixelformat=BA81
-}
-
 # echo 15 > /proc/sys/kernel/printk
 
 test_idx=1
 reinstall_vimc
-simple_topo || exit 1
+simple_topo mdev || exit 1
 echo "===================================================================================="
 echo "Test $test_idx: make sure that the deivce can be plugged and all captures can stream"
 echo "===================================================================================="
@@ -131,7 +41,7 @@ if [ "$out" != $STRM_OUT ]; then echo "streaming sca failed"; exit; fi
 
 
 reinstall_vimc
-simple_topo || exit 1
+simple_topo mdev || exit 1
 echo "=========================================================================="
 echo "Test $test_idx: make sure it is impossible to remove the scaler"
 echo "=========================================================================="
@@ -141,7 +51,7 @@ rmdir "/configfs/vimc/mdev/vimc-scaler:sca" && exit 1
 echo 1 > /configfs/vimc/mdev/hotplug || exit 1
 
 reinstall_vimc
-simple_topo || exit 1
+simple_topo mdev || exit 1
 echo "=========================================================================="
 echo "Test $test_idx: remove the two links [debayer -> scaler],[scaler->vimc-cpature] and the scaler"
 echo "and make sure that the cap-sca can't be streamed and that the the other captures can"
@@ -164,7 +74,7 @@ out=$(v4l2-ctl --stream-mmap --stream-count=$STRM_CNT -d $VIDSCA 2>&1)
 if [ "$out" == $STRM_OUT ]; then echo "streaming sca DID NOT fail (it should have)"; exit; fi
 
 reinstall_vimc
-simple_topo || exit 1
+simple_topo mdev || exit 1
 echo "=========================================================================="
 echo "Test $test_idx: remove the scaler and its links and create it again and make"
 echo "sure the device can be plugged and stream"
@@ -197,16 +107,16 @@ out=$(v4l2-ctl --stream-mmap --stream-count=$STRM_CNT -d $VIDSCA 2>&1)
 if [ "$out" != $STRM_OUT ]; then echo "streaming sca failed"; exit; fi
 
 reinstall_vimc
-simple_topo || exit 1
+simple_topo mdev || exit 1
 echo "==============================================================================="
 echo "Test $test_idx: create two simple devices and make sure that they can both be loaded and upstreamed together"
 echo "==============================================================================="
 ((test_idx++))
 echo 1 > /configfs/vimc/mdev/hotplug || exit 1
-simpler_topo || exit 1
+simpler_topo mdev2 || exit 1
 echo 1 > /configfs/vimc/mdev2/hotplug || exit 1
 configure_all_formats
-configure_all_formats1
+configure_all_formats_simpler
 out=$(v4l2-ctl --stream-mmap --stream-count=$STRM_CNT -d $VIDSEN 2>&1)
 if [ "$out" != $STRM_OUT ]; then echo "streaming sen failed"; exit; fi
 
